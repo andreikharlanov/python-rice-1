0629511a547d1ad76026c6904306cc49f581e140
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2016-01-31 01:40:07 +0700

    Make minimal version of 'Guess the number' game

--- mini-project-2-guess-the-number-game.py
+++ mini-project-2-guess-the-number-game.py
@@ -2,19 +2,20 @@
 # input will come from buttons and an input field
 # all output for the game will be printed in the console
 
+# http://www.codeskulptor.org/#user41_P8vFTOWn4gbjoIo.py
+
 import simplegui
+import random
 
 # helper function to start and restart the game
 def new_game():
-    # initialize global variables used in your code here
-
-    # remove this when you add your code
-    pass
-
+    global secret_number
+    secret_number = random.randrange(0, 100)
 
 # define event handlers for control panel
 def range100():
     # button that changes the range to [0,100) and starts a new game
+
 
     # remove this when you add your code
     pass
@@ -25,10 +26,13 @@ def range1000():
     pass
 
 def input_guess(guess):
-    # main game logic goes here
-
-    # remove this when you add your code
-    pass
+    print "Guess was", int(guess)
+    if int(guess) < secret_number:
+        print "Lower"
+    elif int(guess) > secret_number:
+        print "Higher"
+    else:
+        print "Correct"
 
 
 
