71c0c6982c80215934b1e86eeb604157808700ec
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2016-01-31 02:51:48 +0700

    Add a limit for the number of guesses

--- mini-project-2-guess-the-number-game.py
+++ mini-project-2-guess-the-number-game.py
@@ -2,7 +2,7 @@
 # input will come from buttons and an input field
 # all output for the game will be printed in the console
 
-# http://www.codeskulptor.org/#user41_P8vFTOWn4gbjoIo.py
+# http://www.codeskulptor.org/#user41_7nhiRwmDpFKWLzZ_0.py
 
 import simplegui
 import random
@@ -10,39 +10,76 @@ import random
 # helper function to start and restart the game
 
 range = 100
+remaining_guesses = 7
 
 def new_game():
+    global remaining_guesses
     global secret_number
     global range
 
-    print "New game started."
+    print "\nNew game started."
     print "Range is [0, %d)" % range
+    print "You have %d remaining guesses." % remaining_guesses
 
     secret_number = random.randrange(0, int(range))
     # print secret number for testing
-    print "Secret number is:", secret_number
+    # print "Secret number is:", secret_number
 
 # define event handlers for control panel
 def range100():
     global range
+    global remaining_guesses
+
     range = 100
+    remaining_guesses = 7
+
     new_game()
 
 def range1000():
     global range
+    global remaining_guesses
+
     range = 1000
+    remaining_guesses = 10
+
     new_game()
 
 def input_guess(guess):
-    print "Guess was", int(guess)
-    if int(guess) < secret_number:
+    global remaining_guesses
+
+    remaining_guesses -= 1
+
+    print "\nGuess was", int(guess)
+
+    if secret_number < int(guess):
         print "Lower"
-    elif int(guess) > secret_number:
+        print "\nYou have %d remaining guesses." % remaining_guesses
+    elif secret_number > int(guess):
         print "Higher"
+        print "\nYou have %d remaining guesses." % remaining_guesses
     else:
         print "Correct"
+        if range == 100:
+            remaining_guesses = 7
+        elif range == 1000:
+            remaining_guesses = 10
+        new_game()
+
+    if remaining_guesses == 0:
+        if range == 100:
+            remaining_guesses = 7
+        elif range == 1000:
+            remaining_guesses = 10
+        new_game()
 
 def restart_button():
+    global range
+    global remaining_guesses
+
+    if range == 100:
+        remaining_guesses = 7
+    elif range == 1000:
+        remaining_guesses = 10
     new_game()
 
 # create frame
@@ -61,6 +98,3 @@ frame.start()
 
 # call new_game
 new_game()
-
-
-# always remember to check your completed program against the grading rubric
