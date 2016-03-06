1a99acd5963cd61953a481ae6d016f4ea60b7dd3
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2016-03-06 20:36:11 +0700

    Define global variables

--- mini-project-6-blackjack.py
+++ mini-project-6-blackjack.py
@@ -13,6 +13,16 @@ CARD_BACK_SIZE = (72, 96)
 CARD_BACK_CENTER = (36, 48)
 card_back = simplegui.load_image("http://storage.googleapis.com/codeskulptor-assets/card_jfitz_back.png")
 
+# initialize some useful global variables
+in_play = False
+outcome = ""
+score = 0
+
+# define globals for cards
+SUITS = ('C', 'S', 'H', 'D')
+RANKS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K')
+VALUES = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':10, 'Q':10, 'K':10}
+
 # define card class
 class Card:
     def __init__(self, suit, rank):
