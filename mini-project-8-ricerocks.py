0498bee9ae2f8bbb8a36e17c54666041a2c317a1
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2016-03-18 22:01:21 +0700

    Change CodeSculptor URL

--- mini-project-8-ricerocks.py
+++ mini-project-8-ricerocks.py
@@ -1,5 +1,5 @@
 # My version of RiceRocks game
-# http://www.codeskulptor.org/#user41_b4nrSBkps4_21.py
+# http://www.codeskulptor.org/#user41_b4nrSBkps4_23.py
 import simplegui
 import math
 import random
